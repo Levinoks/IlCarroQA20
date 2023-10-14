6f5667ae3dab93b0a5091eb9e7f37cca7df12bca
Levinoks <qa.94.tester@gmail.com>
2023-10-14 20:42:47 +0300

    run group of tests adding annotation AfterMethod and BeforeMethod

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -13,51 +13,52 @@ public class LoginTests extends BaseTest {
 
         logoutIfLogin();
 
-   }
-   @AfterMethod
-   public void postconditions(){
-       app.getUserHelper().pause(3);
-        clickOkIfRegistered();
-       app.getUserHelper().pause(3);
-        app.getUserHelper().refresh();
-       app.getUserHelper().pause(3);
-
-   }
-
-
-        @Test
-        public void positiveLoginUserDTO () {
-            UserDTO userDTO = new UserDTO("qwerty1@gmail.com", "User#12345");
-            app.getUserHelper().login(userDTO);
-            Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
-
-        }
-
-        @Test
-        public void positiveLoginUserDTOWith () {
-            UserDTOWith userDTOWith = new UserDTOWith().withEmail("qwerty1@gmail.com").withPassword("User#12345");
-            app.getUserHelper().login(userDTOWith);
-            Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
-
-        }
-
-
-        @Test
-        public void positiveLogin () {
-
-            app.getUserHelper().loginUserDTOLombok(userLombok);
-            Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
-
-
-        }
-
-
-        @Test
-        public void negativeLoginPasswordWithoutSymbol () {
-            UserDTOLombok userLombok = UserDTOLombok.builder().email("qwerty1@gmail.com").password("Uuser12345").build();
-            app.getUserHelper().loginUserDTOLombok(userLombok);
-            Assert.assertTrue(app.getUserHelper().validatePopUpMessageLoginIncorrect());
-
-        }
     }
+
+    @AfterMethod
+    public void postconditions() {
+        app.getUserHelper().pause(3);
+        clickOkIfRegistered();
+        app.getUserHelper().pause(3);
+        app.getUserHelper().refresh();
+        app.getUserHelper().pause(3);
+
+    }
+
+
+    @Test
+    public void positiveLoginUserDTO() {
+        UserDTO userDTO = new UserDTO("qwerty1@gmail.com", "User#12345");
+        app.getUserHelper().login(userDTO);
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
+
+    }
+
+    @Test
+    public void positiveLoginUserDTOWith() {
+        UserDTOWith userDTOWith = new UserDTOWith().withEmail("qwerty1@gmail.com").withPassword("User#12345");
+        app.getUserHelper().login(userDTOWith);
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
+
+    }
+
+
+    @Test
+    public void positiveLogin() {
+
+        app.getUserHelper().loginUserDTOLombok(userLombok);
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
+
+
+    }
+
+
+    @Test
+    public void negativeLoginPasswordWithoutSymbol() {
+        UserDTOLombok userLombok = UserDTOLombok.builder().email("qwerty1@gmail.com").password("Uuser12345").build();
+        app.getUserHelper().loginUserDTOLombok(userLombok);
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageLoginIncorrect());
+
+    }
+}
 
